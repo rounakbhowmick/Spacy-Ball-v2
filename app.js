306569a67f0185a6fb825e4038e490db0f6e2901
Rounak Bhowmick <rounakchakda@gmail.com>
2020-06-12 19:10:00 +0530

    design changed and collision issue solved

--- app.js
+++ app.js
@@ -10,16 +10,19 @@ const pokemon = document.querySelector(".pokemon");
 let boundPokemon = pokemon.getBoundingClientRect();
 const finalscore = document.querySelector(".final-score");
 const gameover = document.querySelector(".gameover");
+const logo = document.querySelector(".logo");
+const retry = document.querySelector(".retry");
 let boundContainer = container.getBoundingClientRect();
 //Event Listener
 btn.addEventListener("click", startGame);
+retry.addEventListener("click", startGame);
 document.addEventListener("keydown", pressKeyOn);
 document.addEventListener("keyup", pressKeyOff);
 //Player Object
 let player = {
     score: 0,
     redballs: 1,
-    whiteballs: 1,
+    whiteballs: 100,
     inPlay: false,
     speed: 5
 };
@@ -35,7 +38,6 @@ let keys = {
 
 function pressKeyOn(event) {
     event.preventDefault();
-    //Making true only when 
     keys[event.key] = true;
 }
 
@@ -47,24 +49,28 @@ function pressKeyOff(event) {
 
 
 function startGame() {
+    document.querySelector(".topbar").style.opacity = "10";
     message.style.display = "none";
     finalscore.style.display = "none";
     gameover.style.display = "none"
     btn.style.display = "none";
     pokemon.style.display = "block";
+    logo.style.display = "none"
     player.score = 0;
     player.redballs = 1;
     player.whiteballs = 2;
     player.inPlay = true;
+    retry.style.display = "none";
     scoreupdate();
-    setupballs(2);
+    setupballs(4);
     requestAnimationFrame(playGame);
+    //
+
     //Launch animaion
 }
 
 function playGame() {
     if (player.inPlay == true) {
-
         if (keys.ArrowDown && boundPokemon.y < (boundContainer.height - boundPokemon.height - 20)) {
             boundPokemon.y += player.speed;
         }
@@ -77,8 +83,8 @@ function playGame() {
         }
         pokemon.style.left = boundPokemon.x + "px";
         pokemon.style.top = boundPokemon.y + "px";
-        console.log(pokemon);
         //console.log(pokemon);
+
         requestAnimationFrame(playGame);
 
         let tempEnemy = document.querySelectorAll(".redballs");
@@ -98,11 +104,13 @@ function bgMover(e) {
     //console.log(boundContainer.width);
     if (e.x > boundContainer.width - 100) {
         e.x = 100;
-        e.y = Math.floor(Math.random() * boundContainer.height - 100);
-        if ((e.y > boundContainer.height - 100) || (e.y < 0)) e.y = 100;
+        e.y = Math.floor(Math.random() * boundContainer.height - 75);
+        if (e.y == 0) e.y = 0;
+        if ((e.y > boundContainer.height - 75) || (e.y < 0)) e.y = 100;
         e.style.top = e.y + "px";
     }
     e.style.left = e.x + "px";
+    console.log(e.x, e.y);
     isCollide(pokemon, e);
 
 }
@@ -115,7 +123,6 @@ function setupballs(num) {
 }
 
 function makeWhiteBalls() {
-    //Correct left balls coming
     if (player.whiteballs > 0) {
         let temp = player.whiteballs;
         let div = document.createElement("div");
@@ -131,14 +138,14 @@ function makeWhiteBalls() {
 }
 
 function makeRedBalls() {
-    //Correct left balls coming
+
     if (player.redballs > 0) {
         let temp = player.redballs;
         let div = document.createElement("div");
         div.classList.add("redballs");
-        div.x = Math.floor(Math.random() * 710);
+        div.x = Math.floor(Math.random() * boundContainer.width - 100);
         if (div.x < 0) div.x = 100;
-        div.y = 0.5625;
+        div.y = 0;
         div.speed = Math.ceil(Math.random() * 1) + 3;
         container.appendChild(div);
         div.style.left = div.y + "px";
@@ -149,11 +156,11 @@ function makeRedBalls() {
 function endGame() {
     const whiteballs = document.querySelectorAll(".whiteballs");
     const redballs = document.querySelectorAll(".redballs");
-    finalscore.textContent = `SCORE \n  ${player.score}`;
-
+    finalscore.textContent = `SCORE ${player.score}`;
+    retry.style.display = "block"
     finalscore.style.display = "block";
     gameover.style.display = "block"
-    btn.style.display = "block";
+    document.querySelector(".topbar").style.opacity = "0";
     pokemon.style.display = "none";
 
     whiteballs.forEach(node => {
@@ -169,9 +176,7 @@ function endGame() {
         finalscore.style.color = "yellow";
         localStorage.setItem('score', player.score);
     }
-
     player.inPlay = false;
-
 }
 
 function isCollide(a, b) {
@@ -182,24 +187,15 @@ function isCollide(a, b) {
     //console.log(`pythag:${pythag} calc${(aRect.height / 2 + bRect.width / 2)}`);
     if (pythag < (aRect.height / 2 + bRect.height / 2)) {
         if (b.classList.value === "whiteballs") {
-            console.log(b);
-            bRect.bottom = 0 + "px";
-            bRect.height = 0 + "px";
-            bRect.left = 0 + "px";
-            bRect.right = 0 + "px";
-            bRect.top = 0 + "px";
-            bRect.width = 0 + "px";
-            bRect.x = 0 + "px";
-            bRect.y = 0 + "px";
-            b.top = 0 + "px";
-            b.style = "none";
+            b.style.opacity = "0";
             player.score += 1;
             scoreupdate();
         } else
             endGame();
     }
-
 }
+
+
 //Pythagorus Theorem
 function getDistance(x1, y1, x2, y2) {
     let xdistance = x2 - x1;
